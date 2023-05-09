d8d51d8ee3bce07d69a08ca5877eb732283fde41
Roman Buldygin <froooks@gmail.com>
2023-05-10 02:02:32 +0300

    Just save changes

---
+++ Bld.Libnl.Net/Nl80211/AttributeDataType.cs
@@ -0,0 +1,25 @@
+﻿namespace Bld.Libnl.Net.Nl80211;
+
+public enum NetlinkAttributeType
+{
+    NLA_UNSPEC,
+    NLA_U8,
+    NLA_U16,
+    NLA_U32,
+    NLA_U64,
+    NLA_STRING,
+    NLA_FLAG,
+    NLA_MSECS,
+    NLA_NESTED,
+    NLA_NESTED_ARRAY,
+    NLA_NUL_STRING,
+    NLA_BINARY,
+    NLA_S8,
+    NLA_S16,
+    NLA_S32,
+    NLA_S64,
+    NLA_BITFIELD32,
+    NLA_REJECT,
+    NLA_BE16,
+    NLA_BE32,
+};
---
+++ Bld.Libnl.Net/Nl80211/NlaPolicyDescription.cs
@@ -0,0 +1,6 @@
+﻿namespace Bld.Libnl.Net.Nl80211;
+
+public class NlaPolicyDescription
+{
+    public NetlinkAttributeType Type { get; set; }
+}
