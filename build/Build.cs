847573203bcfff1acf4e663a39e4806b3fbb985f
Roman Buldygin <froooks@gmail.com>
2023-05-20 16:47:01 +0300

    Nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,16 +1,21 @@
-using System;
 using System.Linq;
+
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MinVer;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.Push, },
+    InvokedTargets = new[] { nameof(Clean), nameof(PublishToNuget) },
+    AutoGenerate = true,
+    FetchDepth = 0,
+    ImportSecrets = new[] { "NUGET_API_KEY" })]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -24,21 +29,66 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    public Build()
+    {
+        OutputPath = RootDirectory / "out";
+    }
+
+    [Parameter]
+    readonly string NugetApiKey;
+
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+
+    [MinVer]
+    readonly MinVer MinVer;
+
+    readonly AbsolutePath OutputPath;
+
     Target Clean => _ => _
-        .Before(Restore)
         .Executes(() =>
         {
+            OutputPath.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(settings => settings
+                .SetProjectFile(Solution.Bld_Libnl_Net));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(settings => settings
+                .SetNoRestore(true)
+                .SetProjectFile(Solution.Bld_Libnl_Net)
+                .SetConfiguration(Configuration));
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(settings => settings
+                .SetConfiguration(Configuration)
+                .SetNoBuild(true)
+                .SetProject(Solution.Bld_Libnl_Net)
+                .SetOutputDirectory(OutputPath)
+                .SetVersion(MinVer.PackageVersion));
+        });
+
+    Target PublishToNuget => _ => _
+        .DependsOn(Publish)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(settings => settings
+                .SetApiKey(NugetApiKey)
+                .SetTargetPath(OutputPath.GlobFiles("*.nupkg").First())
+                .SetSource("https://api.nuget.org/v3/index.json"));
         });
 
 }
